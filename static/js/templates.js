f9cf65d920ee51ea5d0590c11f06f8f870833c00
bijan ebrahimy <myopensourceprojects@gmail.com>
2013-07-30 16:21:16 +0430

    added templates.js

---
+++ static/js/templates.js
@@ -0,0 +1,20 @@
+crow_template = {
+    status_form: function(){
+        return '<div class="status_form">\
+                    <div class="btn-toolbar">\
+                        <div class="btn-group">\
+                            <button class="btn btn-mini btn_status_ltr active"><i class="icon icon-align-left"></i></button>\
+                            <button class="btn btn-mini btn_status_length">140</button>\
+                            <button class="btn btn-mini btn_status_rtl"><i class="icon icon-align-right"></i></button>\
+                        </div>\
+                        <div class="btn-group">\
+                            <button class="btn btn-mini btn_status_share"><i class="icon icon-share"></i></button>\
+                        </div>\
+                        <div class="btn-group">\
+                            <button class="btn btn-mini btn_status_upload"><i class="icon  icon-picture"></i></button>\
+                        </div>\
+                    </div>\
+                    <textarea placeholder="Your Status"></textarea>\
+                </div>'
+    }
+}
