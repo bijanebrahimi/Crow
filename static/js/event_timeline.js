b175611d58f1393463e968045f52c65b365548f7
bijan ebrahimy <myopensourceprojects@gmail.com>
2013-08-03 20:30:36 +0430

    version bumped to 0.1

--- static/js/event_timeline.js
+++ static/js/event_timeline.js
@@ -1,3 +1,19 @@
+// This file is part of Crow.
+// Copyright (C) 2013 Bijan Ebrahimi <bijanebrahimi@lavabit.com>
+// 
+// Crow is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Crow is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Crow.  If not, see <http://www.gnu.org/licenses/>.
+
 // Crow navbar scroll
 (function ($) {
     $(function(){
